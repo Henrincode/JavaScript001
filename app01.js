f21f992a3d35ec32dea38ed2470416ee83e56ce6
Henrique inCode <henrincode@gmail.com>
2025-05-08 20:16:51 -0300

    operadores lógicos

--- app01.js
+++ app01.js
@@ -27,10 +27,32 @@ console.log(typeof salario)
 console.log(typeof fumante)
 
 // Operadores
-//     Operador Atribuição
-//     Operadores aritméticos
-//     Operadores relacionais
-//     Operadores lógicos
+//     Operador Atribuição ( = )
+//     Operadores aritméticos ( +, -, /, *)
+console.log(10 + 5)
+
+let n1 = 10, n2 = 5
+
+console.log("soma", n1 + n2) // soma
+console.log("subtração", n1 - n2) // subtração
+console.log("multiplicação", n1 * n2) // multiplicação
+console.log("divisão", n1 / n2) // divisão
+console.log("módulo", n1 % n2) // módulo
+
+//     Operadores relacionais8 ( >, <, >=, <=, !=, ==)
+console.log(n1 == n2) // igualdade
+console.log(n1 != n2) // diferente
+console.log(n1 > n2) // maior
+console.log(10 < 10) // menor
+console.log(10 <= 10) // menor ou igual
+console.log(150 >= 175) // maior ou igual
+
+//     Operadores lógicos (!, &&, ||)
+console.log(!10 > 2) 
+console.log(!false)
+
+console.log(10 > 2 && 35 < 100 && n1 > n2 && 100 < 90) // E - todas as verificações precisam ser verdadeiras para o resultado ser verdadeiro
+console.log(10 < 2 || 100 == 150 || 57 == 57) // OU - apenas uma verificação precisa ser verdadeira para o resultado ser verdadeiro, o resultado só será falso quando TODAS as verificações forem falsas
 
 // Estrutura
 //     Estrutura de controle/decisão
